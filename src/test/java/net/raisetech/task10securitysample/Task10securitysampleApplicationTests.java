aedcf5ee6dbd24d79bd9da54b84fef260da498d4
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-11-05 22:14:01 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/net/raisetech/task10securitysample/Task10securitysampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.raisetech.task10securitysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task10securitysampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
